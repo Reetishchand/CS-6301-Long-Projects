957cd74c74112f0963802fe7a91ba65352bae14a
msd-7 <aircel998@gmail.com>
2021-02-03 23:28:27 -0600

    added methods

---
+++ LP-1/rxg190006/Num.java
@@ -0,0 +1,19 @@
+package rxg190006;
+public class Num {
+
+	public Num(String s) {}
+	public Num(long x) {}
+	public Num add(Num a, Num b) {}
+	public Num subtract(Num a, Num b) {}
+	public Num product(Num a, Num b) {}
+	public Num power(Num a, long n) {}
+	public Num divide(Num a, Num b) {}
+	public Num mod(Num a, Num b) {}
+	public Num squareRoot(Num a) {}
+	public void printList() {}
+	public Num evaluatePostfix(String[] expr);
+	public Num evaluateExp(String[] expr);
+	
+	
+	
+}
